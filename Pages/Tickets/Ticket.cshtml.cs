910f756717948cc4e890ada9466649791c65bcd0
relyq <nicoale_mainenti@hotmail.es>
2022-04-22 14:01:12 -0300

    close ticket button

--- Pages/Tickets/Ticket.cshtml.cs
+++ Pages/Tickets/Ticket.cshtml.cs
@@ -21,6 +21,7 @@ namespace Tracker.Pages.Tickets
         }
 
         public Ticket Ticket { get; set; }
+        public int Id { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -41,6 +42,58 @@ namespace Tracker.Pages.Tickets
                 return NotFound();
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? Id)
+        {
+            if(Id == null)
+            {
+                return NotFound();
+            }
+
+            Ticket = await _context.Ticket
+                .Include(t => t.Assignee)
+                .Include(t => t.Project)
+                .Include(t => t.Status)
+                .Include(t => t.Submitter)
+                .Include(t => t.Type).FirstOrDefaultAsync(m => m.Id == Id);
+
+            if(Ticket == null)
+            {
+                return NotFound();
+            }
+            if(Ticket.TicketStatusId == 2)
+            {
+                return Page();
+            }
+
+            Ticket.TicketStatusId = 2;
+            Ticket.Closed = DateTime.Now;
+
+            _context.Attach(Ticket).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TicketExists(Ticket.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return await OnGetAsync(Ticket.Id);
+        }
+
+        private bool TicketExists(int id)
+        {
+            return _context.Ticket.Any(e => e.Id == id);
         }
     }
 }
