b7873c86698bc7afa40bb7192d1fac1dafe8f726
relyq <relyqx@gmail.com>
2022-10-04 10:20:43 -0300

    rolescontroller

---
+++ Controllers/RolesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tracker.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Tracker.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RolesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<RolesController>
+        [HttpGet]
+        public async Task<IEnumerable<string>> Get()
+        {
+            var roles = await _context.Roles.ToListAsync();
+
+            List<string> rolesNames = new List<string>();
+            
+            roles.ForEach((role) => { rolesNames.Add(role.Name); });
+
+            return rolesNames;
+        }
+
+        // GET api/<RolesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<RolesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<RolesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<RolesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
