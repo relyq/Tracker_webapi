8462dc3dfa1a7dc8ff17361d503d6b65c687ea8e
relyq <relyqx@gmail.com>
2023-05-10 23:14:16 -0300

    fixed perms on aws post install

--- .aws/post_install.sh
+++ .aws/post_install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+chown -R tracker:tracker /opt/tracker/api
+chmode -R 0755 /opt/tracker/api
 mv /opt/tracker/api/appsettings.aws.json /opt/tracker/api/appsettings.json
 mv /opt/tracker/api/appsettings.aws.Development.json /opt/tracker/api/appsettings.Development.json
 chmod +x /opt/tracker/api/scripts/*.py
